a25cb06c8407a6a07717412f20b66ccfd3c1bba8
sadeghbiglar <sbmail555@gmail.com>
2025-03-24 12:16:20 +0330

    change auth styles link

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ isset($title) ? $title.' - '.config('app.name') : config('app.name') }}</title>
+    <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="auth-layout">
