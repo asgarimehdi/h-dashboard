8d9e4233c797f1b17c7ba557e1e2805bcc124554
unknown <mehdi.asgari@gmail.com>
2025-03-30 23:36:48 +0330

    change county map and theme

--- resources/views/components/theme-selector.blade.php
+++ resources/views/components/theme-selector.blade.php
@@ -1,11 +1,12 @@
-<x-dropdown class="btn-sm" label="Theme" title="Theme" icon="o-swatch" >
-    <x-slot:trigger>
-        <x-button icon="o-swatch" class="btn-circle btn-outline" />
-    </x-slot:trigger>
-    <x-input type="radio" name="theme-dropdown" class="theme-controller w-full btn btn-sm  btn-ghost justify-start" aria-label="Luxury" value="luxury" />
-    <x-input type="radio" name="theme-dropdown" class="theme-controller w-full btn btn-sm  btn-ghost justify-start" aria-label="Valentine" value="valentine" />
-    <x-input type="radio" name="theme-dropdown" class="theme-controller w-full btn btn-sm  btn-ghost justify-start" aria-label="Cupcake" value="cupcake" />
-    <x-input type="radio" name="theme-dropdown" class="theme-controller w-full btn btn-sm  btn-ghost justify-start" aria-label="Aqua" value="aqua" />
-    <x-input type="radio" name="theme-dropdown" class="theme-controller w-full btn btn-sm  btn-ghost justify-start" aria-label="Dark" value="dark" />
-    <x-input type="radio" name="theme-dropdown" class="theme-controller w-full btn btn-sm  btn-ghost justify-start" aria-label="Light" value="light" />
-</x-dropdown>
+{{--<x-dropdown class="btn-sm" label="Theme" title="Theme" icon="o-swatch" >--}}
+{{--    <x-slot:trigger>--}}
+{{--        <x-button icon="o-swatch" class="btn-circle btn-outline" />--}}
+{{--    </x-slot:trigger>--}}
+{{--    <x-input type="radio" name="theme-dropdown" class="theme-controller w-full btn btn-sm  btn-ghost justify-start" aria-label="Luxury" value="luxury" />--}}
+{{--    <x-input type="radio" name="theme-dropdown" class="theme-controller w-full btn btn-sm  btn-ghost justify-start" aria-label="Valentine" value="valentine" />--}}
+{{--    <x-input type="radio" name="theme-dropdown" class="theme-controller w-full btn btn-sm  btn-ghost justify-start" aria-label="Cupcake" value="cupcake" />--}}
+{{--    <x-input type="radio" name="theme-dropdown" class="theme-controller w-full btn btn-sm  btn-ghost justify-start" aria-label="Aqua" value="aqua" />--}}
+{{--    <x-input type="radio" name="theme-dropdown" class="theme-controller w-full btn btn-sm  btn-ghost justify-start" aria-label="Dark" value="dark" />--}}
+{{--    <x-input type="radio" name="theme-dropdown" class="theme-controller w-full btn btn-sm  btn-ghost justify-start" aria-label="Light" value="light" />--}}
+{{--</x-dropdown>--}}
+<x-theme-toggle darkTheme="coffee" lightTheme="cupcake" />
