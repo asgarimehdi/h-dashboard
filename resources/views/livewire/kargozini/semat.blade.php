f6b66d96d1c1f69068ac0a2a1b4c1a48c6a67e34
unknown <mehdi.asgari@gmail.com>
2025-04-07 12:29:33 +0330

    refactor semat, modal base

--- resources/views/livewire/kargozini/semat.blade.php
+++ resources/views/livewire/kargozini/semat.blade.php
@@ -1,21 +1,20 @@
 <?php
 
-
 use App\Models\Semat;
 use Livewire\Volt\Component;
 use Mary\Traits\Toast;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Livewire\WithPagination;
 
-new class extends Component
-{
+new class extends Component {
     use WithPagination;
     use Toast;
+
     public $name;
     public int|null $editingId = null;
     public string $search = '';
     public int $perPage = 5;
-    public bool $drawer = false;
+    public bool $modal = false;
 
     public array $sortBy = ['column' => 'id', 'direction' => 'asc'];
 
@@ -23,19 +22,20 @@ new class extends Component
     public function clear(): void
     {
         $this->reset();
-        $this->success('Filters cleared.', position: 'toast-bottom');
+        $this->info('فیلدها خالی شدند', position: 'toast-bottom');
     }
 
     // Delete action
     public function delete(Semat $semat): void
     {
         try {
-        $semat->delete();
-        $this->warning("$semat->name حذف شد ", 'با موفقیت', position: 'toast-bottom');
+            $semat->delete();
+            $this->warning("$semat->name حذف شد ", 'با موفقیت', position: 'toast-bottom');
         } catch (\Exception $e) {
             $this->error("امکان حذف وجود ندارد زیرا در جدول دیگری استفاده شده است.", position: 'toast-bottom');
         }
     }
+
     // create action
     public function createSemat(Semat $semat): void
     {
@@ -47,31 +47,37 @@ new class extends Component
 
         $this->success("$this->name ایجاد شد ", 'با موفقیت', position: 'toast-bottom');
         $this->reset(['name']);
+        $this->modal = false;
     }
 
     //edit clicked
     public function editSemat($id)
     {
-        //dd($id);
         $semat = Semat::findOrFail($id);
         $this->editingId = $id;
         $this->name = $semat->name;
     }
-//edit action
-    public function updateSemat()
+
+    //edit action
+    public function updateSemat(): void
     {
         $this->validate([
             'name' => 'required|string|max:255|unique:semats,name,' . $this->editingId,
         ]);
 
-        $semat = Semat::findOrFail($this->editingId);
-        $semat->update(['name' => $this->name]);
+        try {
+            $semat = Semat::findOrFail($this->editingId);
+            $semat->update(['name' => $this->name]);
 
-        $this->success("$this->name بروزرسانی شد ", 'با موفقیت', position: 'toast-bottom');
-        $this->reset(['name', 'editingId']);
+            $this->success("$this->name بروزرسانی شد ", 'با موفقیت', position: 'toast-bottom');
+            $this->reset(['name', 'editingId']);
+            $this->modal = false;
+        } catch (\Exception $e) {
+            $this->error("خطا در ویرایش", position: 'toast-bottom');
+        }
     }
-    // Table headers
 
+    // Table headers
     public function headers(): array
     {
         return [
@@ -79,14 +85,6 @@ new class extends Component
             ['key' => 'name', 'label' => 'عنوان', 'class' => 'w-64'],
         ];
     }
-
-    /**
-     * For demo purpose, this is a static collection.
-     *
-     * On real projects you do it with Eloquent collections.
-     * Please, refer to maryUI docs to see the eloquent examples.
-     */
-
 
     public function semats(): LengthAwarePaginator
     {
@@ -97,11 +95,7 @@ new class extends Component
         }
         $query->orderBy(...array_values($this->sortBy));
         return $this->semats = $query->paginate($this->perPage);
-
     }
-
-
-
 
     public function with(): array
     {
@@ -117,62 +111,71 @@ new class extends Component
     <!-- HEADER -->
     <x-header title="مدیریت وضعیت های سمت" separator progress-indicator>
         <x-slot:middle class="!justify-end">
-            <x-input placeholder="Search..." wire:model.live.debounce="search" clearable icon="o-magnifying-glass" />
         </x-slot:middle>
         <x-slot:actions>
-            <x-button class="btn-success btn-sm" label="ثبت جدید" @click="$wire.drawer = true" responsive icon="o-plus" />
 
             <x-theme-selector />
         </x-slot:actions>
-
     </x-header>
 
-    <!-- TABLE  -->
-    <x-card shadow >
-        @if (isset($editingId))
-            <div class="flex items-center space-x-2">
-                <x-input type="text" wire:model="name" class="flex-1 w-lg"  wire:keydown.enter="updateSemat"/>
-                <x-button wire:click="updateSemat" class="btn-success btn-sm" icon="o-check"/>
-                <x-button wire:click="$set('editingId', null)" class="btn-outline btn-sm" icon="o-x-mark"/>
+    <!-- TABLE -->
+    <x-card shadow>
+        <div class="breadcrumbs flex gap-2 items-center">
+            <x-button class="btn-success" @click="$wire.modal = true" responsive icon="o-plus"/>
+            <div class="flex-1">
+                <x-input
+                    placeholder="Search..."
+                    wire:model.live.debounce="search"
+                    clearable
+                    icon="o-magnifying-glass"
+                    class="w-full"
+                />
             </div>
-        @endif
+        </div>
         <x-table :headers="$headers" :rows="$semats" :sort-by="$sortBy" with-pagination per-page="perPage"
-                 :per-page-values="[3, 5, 10]" @class(['opacity-10' => isset($editingId)])>>
+                 :per-page-values="[3, 5, 10]">
             @foreach($semats as $semat)
-                <tr wire:key="{{ $semat->id }}" >
+                <tr wire:key="{{ $semat->id }}">
                     <td>{{ $semat->id }}</td>
-
                     <td>{{ $semat->name }}</td>
                     <td>
                         @scope('actions', $semat)
-                        <!-- دکمه ویرایش -->
-                        <x-button icon="o-pencil" wire:click="editSemat({{ $semat->id }})"
-                                  class="btn-ghost btn-sm text-primary" label="Edit" />
+                        <x-button icon="o-pencil"
+                                  wire:click="editSemat({{ $semat->id }})"
+                                  class="btn-ghost btn-sm text-primary"
+                                  @click="$wire.modal = true">
+                            <span class="hidden sm:inline">ویرایش</span>
+                        </x-button>
 
-                        <!-- دکمه حذف -->
-                        <x-button icon="o-trash" wire:click="delete({{ $semat->id }})"
-                                  wire:confirm="Are you sure?" spinner
-                                  class="btn-ghost btn-sm text-error" label="Delete" />
-
-
+                        <x-button icon="o-trash"
+                                  wire:click="delete({{ $semat->id }})"
+                                  wire:confirm="Are you sure?"
+                                  spinner
+                                  class="btn-ghost btn-sm text-error">
+                            <span class="hidden sm:inline">حذف</span>
+                        </x-button>
                         @endscope
                     </td>
                 </tr>
             @endforeach
         </x-table>
-
     </x-card>
 
-    <!-- FILTER DRAWER -->
-    <x-drawer wire:model="drawer" title="ثبت جدید" left separator with-close-button class="lg:w-1/3">
-        <form wire:submit.prevent="createSemat" class="space-y-4">
-            <x-input wire:model="name"
-                     label="عنوان سمت"
-                     placeholder="عنوان"
-                     required icon="o-magnifying-glass" @keydown.enter="$wire.drawer = false" />
-            <x-button type="submit"  label="ایجاد" icon="o-check" class="btn-primary"  spinner />
-            <x-button label="ریست" icon="o-x-mark" wire:click="clear"  spinner/>
+    <!-- MODAL -->
+    <x-modal wire:model="modal" :title="$editingId ? 'ویرایش عنوان سمت' : 'ثبت عنوان سمت جدید'" persistent separator>
+        <x-form wire:submit.prevent="{{ $editingId ? 'updateSemat' : 'createSemat' }}" class="grid gap-4">
+            <x-input
+                wire:model="name"
+                label="عنوان سمت"
+                placeholder="عنوان"
+                required
+                icon="o-magnifying-glass"
+            />
 
-        </form>
-    </x-drawer>
+            <div class="flex gap-4">
+                <x-button type="submit" label="ذخیره" icon="o-check" class="btn-primary pl-6" spinner />
+                <x-button label="ریست" icon="o-x-mark" wire:click="clear" class="btn-default pl-6" spinner/>
+            </div>
+        </x-form>
+    </x-modal>
 </div>
